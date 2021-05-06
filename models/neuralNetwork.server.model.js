6f2b5c4f947976e4fed8e88399442d6cc6e07717
tracycollins <tc@threeceemedia.com>
2021-05-05 20:18:05 -0400

    package

--- models/neuralNetwork.server.model.js
+++ models/neuralNetwork.server.model.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 exports.NeuralNetworkSchema = new Schema({
-	id: { type: String, unique: true, lowercase: true },
-  networkInput: { type: Schema.Types.ObjectId, ref: 'NetworkInput' },
+  id: { type: String, unique: true, lowercase: true },
+  networkInput: { type: Schema.Types.ObjectId, ref: "NetworkInput" },
   networkTechnology: { type: String, default: "tensorflow" },
   tensorflowModelPath: { type: String },
   networkFamily: { type: String },
@@ -11,10 +11,11 @@ exports.NeuralNetworkSchema = new Schema({
   network: { type: mongoose.Schema.Types.Mixed, default: {} },
   networkJson: { type: mongoose.Schema.Types.Mixed, default: {} },
   numInputs: { type: Number, default: 0 },
+  image: { type: mongoose.Schema.Types.Mixed },
   hiddenLayerSize: { type: Number, default: 0 },
   numOutputs: { type: Number, default: 0 },
   outputs: { type: mongoose.Schema.Types.Mixed, default: {} },
   fit: { type: mongoose.Schema.Types.Mixed, default: {} },
   test: { type: mongoose.Schema.Types.Mixed, default: {} },
-  meta: { type: mongoose.Schema.Types.Mixed, default: {} }
+  meta: { type: mongoose.Schema.Types.Mixed, default: {} },
 });
